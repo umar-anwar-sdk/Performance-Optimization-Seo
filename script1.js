0d1629254c0da7abd9a981ea7e460050a517f994
umar-anwar-sdk <146811262+umar-anwar-sdk@users.noreply.github.com>
2023-10-18 11:43:24 +0500

    SEO tools using

---
+++ script1.js
@@ -0,0 +1,2 @@
+// Example JavaScript for script1.js
+document.addEventListener("DOMContentLoaded", function() { console.log("Script 1 has loaded.") });
---
+++ script2.js
@@ -0,0 +1,2 @@
+// Example JavaScript for script2.js
+document.addEventListener("DOMContentLoaded", function() { console.log("Script 2 has loaded.") });
